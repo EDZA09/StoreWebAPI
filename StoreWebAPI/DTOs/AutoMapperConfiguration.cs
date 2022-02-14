2831447581d7f0587d651acb9c5ab5d179e85a05
EDZA09 <eduarstiven2001@gmail.com>
2022-02-13 21:39:21 -0500

    Primer Commit

---
+++ StoreWebAPI/DTOs/AutoMapperConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using StoreWebAPI.Models;
+
+namespace StoreWebAPI.DTOs
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Customer, CustomerDTO>()
+                   .ForMember(x => x.CustomerOrder, o => o.Ignore())
+                   //.ForMember(x => x.Nombre, o => o.MapFrom(s => s.FirstName))
+                   .ReverseMap();
+
+                cfg.CreateMap<CustomerOrder, CustomerOrderDTO>()
+                   .ForMember(x => x.OrderDetail, o => o.Ignore())
+                   .ReverseMap();
+
+                cfg.CreateMap<Employee, EmployeeDTO>()
+                   .ReverseMap();
+
+                cfg.CreateMap<OrderDetail, OrderDetailDTO>()
+                   .ReverseMap();
+
+                cfg.CreateMap<OrderStatus, OrderStatusDTO>()
+                   .ForMember(x => x.CustomerOrder, o => o.Ignore())
+                   .ReverseMap();
+
+                cfg.CreateMap<Product, ProductDTO>()
+                   .ForMember(x => x.OrderDetail, o => o.Ignore())
+                   .ReverseMap();
+            });
+        }
+    }
+}
---
+++ StoreWebAPI/Startup.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoreWebAPI.DTOs;
+
+namespace StoreWebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            AutoMapperConfiguration.Configure();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddJsonOptions(
+                options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+            services.AddDbContext<Models.StoreAPIDbContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
